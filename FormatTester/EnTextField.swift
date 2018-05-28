5494b7455f174a73666b2d952879e321a4ad3728
Barry P. Medoff <barry@medoff.com>
2018-05-28 16:28:56 -0700

    Accepts and displays numeric input via NumberFormatter() for correct localization

---
+++ FormatTester/EnTextField.swift
@@ -0,0 +1,25 @@
+//
+//  EnTextField.swift
+//  FormatTester
+//
+//  Created by Barry P. Medoff on 5/24/18.
+//  Copyright © 2018 Barry P. Medoff. All rights reserved.
+//
+
+import UIKit
+
+class EnTextField: UITextField {
+
+    override var textInputMode: EnTextInputMode? {
+        return EnTextInputMode()
+    }
+    
+    class EnTextInputMode: UITextInputMode {
+        override var primaryLanguage: String? {
+            return "en-US"
+        }
+    }
+}
+
+
+
--- FormatTester/ViewController.swift
+++ FormatTester/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var frequencyInputField: EnTextField!
+    @IBOutlet weak var frequencyOutputField: UILabel!
+    
+    
+    var inputValue:NSNumber?
+    var formatter: NumberFormatter?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        formatter = NumberFormatter()
+        formatter?.minimumIntegerDigits = 1
+        formatter?.minimumFractionDigits = 3
+        formatter?.maximumFractionDigits = 6
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func saveFrequency(_ sender: UIButton) {
+        frequencyInputField.resignFirstResponder()
+        
+        if let inputString = frequencyInputField.text {
+            inputValue = formatter?.number(from: inputString)
+            if let numberToDisplay = inputValue {
+                frequencyOutputField.text = formatter?.string(from: numberToDisplay)
+            } else {
+                frequencyOutputField.text = "null value"
+            }
+            
+        } else {
+            frequencyOutputField.text = "null value"
+        }
+    }
+    
 }
 
